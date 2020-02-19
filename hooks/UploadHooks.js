ad971978d0fc5da70d79fdb250b95321e8961f26
Ram babu Mishra <ramm.metropolia.fi>
2020-02-19 13:57:19 +0200

    Uploads branch: creating upload.js and uploadHooks.js with upload and…

--- hooks/UploadHooks.js
+++ hooks/UploadHooks.js
@@ -1,83 +1,156 @@
 import { useState, useContext } from "react";
 import { AsyncStorage } from "react-native";
-import { fetchGET } from "./APIHooks";
+import validate from "validate.js";
 import { MediaContext } from "../contexts/MediaContext";
+const apiUrl = "http://media.mw.metropolia.fi/wbma/";
 
-const useUploadForm = () => {
-  const [media, setMedia] = useContext(MediaContext);
+const constraints = {
+  title: {
+    presence: {
+      message: "This field is required"
+    },
+    length: {
+      minimum: 3,
+      message: "Field must be atleast three characters"
+    }
+  },
+  description: {
+    presence: {
+      message: "This field is required"
+    },
+    length: {
+      minimum: 5,
+      message: "Field must be atleast five characters"
+    }
+  }
+};
+
+const useUploadForm = props => {
   const [inputs, setInputs] = useState({});
   const [errors, setErrors] = useState({});
+  const [media, setMedia] = useContext(MediaContext);
 
-  const handleTitleChange = text => {
+  const validateField = (attr, value) => {
+    try {
+      let fieldOK = true;
+      const valResult = validate({ [attr]: value }, constraints);
+      console.log("validateU", valResult);
+      let valid = undefined;
+      if (valResult[attr]) {
+        valid = valResult[attr][0];
+        fieldOK = false;
+      }
+      setErrors(errors => ({
+        ...errors,
+        [attr]: valid
+      }));
+      return fieldOK;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const reset = () => {
+    setInputs(inputs => ({
+      ...inputs,
+      title: "",
+      description: "",
+      image: ""
+    }));
+    setErrors(errors => ({
+      ...errors,
+      title: undefined,
+      description: undefined
+    }));
+  };
+
+  const handleUploadTitleChange = text => {
+    console.log("uploadTitleChangeDetected", text);
     setInputs(inputs => ({
       ...inputs,
       title: text
     }));
   };
 
-  const handleDescriptionChange = text => {
+  const handleUploadDescriptionChange = text => {
+    console.log("uploadDesCh", text);
     setInputs(inputs => ({
       ...inputs,
       description: text
     }));
   };
 
-  const handleUpload = async (file, navigation) => {
-    const filename = file.uri.split("/").pop();
+  const handleUploadImageUri = localUri => {
+    console.log("uploadImgUri", localUri);
+    setInputs(inputs => ({
+      ...inputs,
+      image: localUri
+    }));
+  };
+
+  const handleUpload = async navigation => {
+    const localUri = inputs.image;
+    // ImagePicker saves the taken photo to disk and returns a local URI to it
+    const filename = localUri.split("/").pop();
+
+    // Infer the type of the image
     const match = /\.(\w+)$/.exec(filename);
     let type = match ? `image/${match[1]}` : `image`;
+
     // fix jpg mimetype
     if (type === "image/jpg") {
       type = "image/jpeg";
     }
 
-    const fd = new FormData();
-    fd.append("title", inputs.title);
-    fd.append("description", inputs.description);
-    fd.append("file", { uri: file.uri, name: filename, type });
-
-    console.log("FD:", fd);
-
-    try {
-      const token = await AsyncStorage.getItem("userToken");
+    const token = await AsyncStorage.getItem("userToken");
+    if (token !== null) {
+      // Upload the image using the fetch and FormData APIs
+      const formData = new FormData();
+      // Assume "photo" is the name of the form field the server expects
+      formData.append("file", { uri: localUri, name: filename, type });
+      formData.append("title", inputs.title);
+      formData.append("description", inputs.description);
 
       const fetchOptions = {
         method: "POST",
         headers: {
+          "Content-Type": "multipart/form-data",
           "x-access-token": token
         },
-        body: fd
+        body: formData
       };
-
-      const result = await fetch(
-        "http://media.mw.metropolia.fi/wbma/media",
-        fetchOptions
-      );
-      const json = await result.json();
-      console.log("upload result:", json);
+      const response = await fetch(apiUrl + "media", fetchOptions);
+      const json = await response.json();
+      console.log("upload result", json);
       if (json.file_id) {
         // upload successful
-        const json = await fetchGET("media/all");
+        const response = await fetch(apiUrl + "media/all");
+        const json = await response.json();
+        console.log("apihooks", json);
         const result = await Promise.all(
           json.files.map(async item => {
-            return await fetchGET("media", item.file_id);
+            const response = await fetch(apiUrl + "media/" + item.file_id);
+            return await response.json();
           })
         );
         setMedia(result);
         navigation.push("Home");
       }
-    } catch (e) {
-      console.log(e.message);
+      return json;
+    } else {
+      console.log("token is null");
     }
   };
 
   return {
-    handleTitleChange,
-    handleDescriptionChange,
+    handleUploadTitleChange,
+    handleUploadDescriptionChange,
+    handleUploadImageUri,
     handleUpload,
+    validateField,
+    reset,
     inputs,
-    errors,
-    setErrors
+    errors
   };
 };
 
--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -2,19 +2,19 @@ import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
-import Home from "../views/Home";
-import Profile from "../views/Profile";
-import Single from "../views/Single";
-import AuthLoading from "../views/AuthLoading";
-import Login from "../views/Login";
-import Upload from "../views/Upload";
+import Home from "../views/Home.js";
+import Profile from "../views/Profile.js";
+import Upload from "../views/Upload.js";
+import Single from "../views/Single.js";
+import Login from "../views/Login.js";
+import AuthLoading from "../views/AuthLoading.js";
 import { Icon } from "native-base";
 
 const TabNavigator = createBottomTabNavigator(
   {
     Home,
-    Profile,
-    Upload
+    Upload,
+    Profile
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
@@ -26,47 +26,29 @@ const TabNavigator = createBottomTabNavigator(
         } else if (routeName === "Profile") {
           iconName = "person";
         } else if (routeName === "Upload") {
-          iconName = "add";
+          iconName = "cloud-upload";
         }
-
         // You can return any component that you like here!
         return <Icon name={iconName} size={25} />;
       }
-    }),
-    tabBarOptions: {
-      activeTintColor: "#000"
-    }
+    })
   }
 );
 
-TabNavigator.navigationOptions = ({ navigation }) => {
-  const { routeName } = navigation.state.routes[navigation.state.index];
-
-  // You can do whatever you like here to pick the title based on the route name
-  const headerTitle = routeName;
-
-  return {
-    headerTitle
-  };
-};
-
-const StackNavigator = createStackNavigator(
-  // RouteConfigs
-  {
-    Home: {
-      screen: TabNavigator,
-      navigationOptions: {
-        headerMode: "none" // this will hide the header
-      }
-    },
-    Single: {
-      screen: Single
-    },
-    Logout: {
-      screen: Login
+const StackNavigator = createStackNavigator({
+  Home: {
+    screen: TabNavigator,
+    navigationOptions: {
+      headerMode: "none"
     }
+  },
+  Single: {
+    screen: Single
+  },
+  Logout: {
+    screen: Login
   }
-);
+});
 
 const Navigator = createSwitchNavigator(
   {
@@ -79,4 +61,6 @@ const Navigator = createSwitchNavigator(
   }
 );
 
-export default createAppContainer(Navigator);
+const AppContainer = createAppContainer(Navigator);
+
+export default AppContainer;
